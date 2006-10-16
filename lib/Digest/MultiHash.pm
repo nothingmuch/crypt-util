96e842fc53539424608dd1771195b79ebf72d9fa
Yuval Kogman <nothingmuch@woobling.org>
2006-10-16 16:05:58 +0000

    Improvements + docs for Digest::MultiHash

--- lib/Digest/MultiHash.pm
+++ lib/Digest/MultiHash.pm
@@ -7,28 +7,55 @@ use warnings;
 
 use base qw/Class::Accessor::Fast Digest::base/;
 
+use Carp qw/croak/;
+
 use Digest;
 use Digest::MoreFallbacks;
+use Scalar::Util qw/blessed/;
 
-__PACKAGE__->mk_accessors(qw/__digest_objects width hashes/);
+__PACKAGE__->mk_accessors(qw/_digest_objects width/);
+__PACKAGE__->mk_ro_accessors(qw/hashes/);
 
-sub _digest_objects {
-	my $self = shift;
+sub new {
+	my ( $class, @params ) = @_;
 
-	@{ $self->__digest_objects || $self->_create_digest_objects };
+	if ( @params != 1 ) {
+		croak "You must supply an even sized list or a single hash reference to new" unless @params % 2 == 0;
+		@params = { @params };
+	}
+
+	if ( (ref($params[0])||'') ne "HASH" ) {
+		croak "You must supply an even sized list or a single hash reference to new";
+	}
+
+	my $self = $class->SUPER::new( @params );
+	
+	$self->_create_digest_objects;
+
+	return $self;
+}
+
+sub _call {
+	my ( $self, $method, @args ) = @_;
+	map { $_->$method( @args ) } @{ $self->_digest_objects };
 }
 
 sub _create_digest_objects {
 	my $self = shift;
 
-	my @digests = map { Digest->new($_) } @{ $self->hashes || [qw/SHA-1/] };
+	my @digests = map {
+		blessed($_)
+			? $_
+			: Digest->new(
+				((ref($_)||'') eq "ARRAY")
+					? @$_
+					: $_
+			)
+	} @{ $self->hashes || [qw/SHA-1/] };
 
 	die "No digest module specified" unless @digests;
 
-	@digests = @digests[0 .. 2] if @digests > 3 and !$self->hashes; # if it's defaults limit to 3
-	$self->__digest_objects(\@digests);
-
-	return \@digests;
+	$self->_digest_objects(\@digests);
 }
 
 sub clone {
@@ -36,23 +63,27 @@ sub clone {
 	$self->new({
 		width => $self->width,
 		hashes => $self->hashes,
-		( $self->__digest_objects ? ( __digest_objects => [map { $_->clone } @{ $self->__digest_objects }] ) : () ),
+		_digest_objects => [ $self->_call("clone") ],
 	});
 }
 
 sub add {
 	my ( $self, @args ) = @_;
-	$_->add(@args) for $self->_digest_objects;
+	$self->_call("add", @args);
 }
 
 sub digest {
 	my $self = shift;
+
+	my @digests = $self->_call("digest");
 	
-	my $width = $self->width;
+	my $width = $self->width || length($digests[0]);
 
-	my ( $buf, @pieces ) = unpack "(a$width)*", join "", my @digests = map { $_->digest } $self->_digest_objects;
+	my $concat = join "", @digests;
+	
+	die "Chosen hashes are insufficient for desired width" if length($concat) < $width;
 
-	die "Chosen hashes are insufficient for desired width" if !@pieces and length($buf) < $width;
+	my ( $buf, @pieces ) = unpack "(a$width)*", $concat;
 
 	$buf ^= $_ for @pieces;
 
@@ -67,13 +98,85 @@ __END__
 
 =head1 NAME
 
-Digest::MultiHash - Generalized Digest::SV1
+Digest::MultiHash - XOR based, variable width multiplexing of hashes (a
+generalized Digest::SV1).
 
 =head1 SYNOPSIS
 
 	use Digest::MultiHash;
 
+	my $d = Digest::Multihash->new(
+		width => 16, # bytes
+		hashs => ["SHA-512", "Whirlpool"], # see below for arbitrary arguments
+	);
+
+	$d->add($data);
+
+	print $d->hexdigest;
+
 =head1 DESCRIPTION
+
+This class inherits from L<Digest::base>, and provides generalized digest
+multiplexing.
+
+It will multiplex all calls to C<add> to all of it's sub digest objects.
+Likewise, when the final digest is extracted the digests will be extracted and
+then XOR'd over eachother according to C<width>.
+
+C<width> will default to the width of the first hash if unspecified.
+
+C<hashes> defaults to C<SHA-1> for compatibility reasons.
+
+This module is useful for generating keys from passphrases, by supplying the
+desired width and simply making sure there is enough data from the combined
+hashes.
+
+=head1 METHODS
+
+See L<Digest> for the complete API. This module inherits from L<Digest::base>.
+
+=over 4
+
+=item new
+
+This methods accepts a hash reference or an even sized list of parameters named
+according to the methods.
+
+=item add
+
+=item digest
+
+Compute the hash by calling C<digest> on all of the subhashes, splitting the
+result up into C<width> sized chunk, and then XORing these together.
+
+If the result is not aligned on C<width> the result will not be truncated. The
+shorter string will still be XOR'd with the hash, even if this only affects
+part of the result.
+
+If there are not at least C<width> bytes of data in the output of the combined
+hashes an error is thrown.
+
+=item clone
+
+Clones the hash.
+
+=item hashes
+
+Get the array of hashes to use. Array values in this will be dereferenced
+before the call to L<Digest/new> to allow passing of arbitrary arguments.
+Blessed objects (of any class) will be used verbatim.
+
+The list of hashes cannot be changed after construction.
+
+=item width
+
+Get/set the byte-width to use.
+
+=back
+
+=head1 SEE ALSO
+
+L<Digest>, L<Digest::SV1>, L<Digest::SHA1>
 
 =cut
 
--- t/multihash.t
+++ t/multihash.t
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Test::More;
+use Test::Exception;
 
 use Digest::MultiHash;
 
@@ -12,7 +13,7 @@ BEGIN {
 		unless eval { Digest::MultiHash->new }
 			and $@ !~ /^Can't find any digest module/;
 
-	plan tests => 2;
+	plan tests => 5;
 }
 
 my $d = Digest::MultiHash->new;
@@ -23,8 +24,27 @@ $d->width( 8 );
 
 $d->add("foo bar gorch");
 
-my $d2 = Digest::MultiHash->new({ width => 8 });
+my $d2 = Digest::MultiHash->new( width => 8 );
 
 $d2->add("foo bar moose");
 
-cmp_ok( $d->digest, "ne", $d2->digest, "digests differ" );
+my $hash2 = $d2->digest;
+
+cmp_ok( $d->digest, "ne", $hash2, "digests differ" );
+
+is( length($hash2), 8, "the hash width is 8" );
+
+throws_ok {
+	my $d = Digest::MultiHash->new(
+		width => 1024, # only 20 bytes in sha1
+	);
+
+	$d->add("foo");
+
+	$d->digest;
+} qr/insufficient.*width/, "Insufficient width causes error";
+
+throws_ok {
+	Digest::MultiHash->new( hashes => [] );
+} qr/No digest module specified/, "Can't construct without hashes";
+
