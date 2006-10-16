748601389dbbfe19142a37955847d903ff4eed3e
Yuval Kogman <nothingmuch@woobling.org>
2006-10-16 16:20:35 +0000

    Documentation boilerplate

--- lib/Crypt/Util.pm
+++ lib/Crypt/Util.pm
@@ -925,6 +925,41 @@ qw/SHA1 RIPEMD-160 Whirlpool MD5/
 
 =back
 
+=head1 SEE ALSO
+
+L<Digest>, L<Crypt::CBC>, L<Crypt::CFB>,
+Lhttp://en.wikipedia.org/wiki/Block_cipher_modes_of_operation>.
+
+=head1 VERSION CONTROL
+
+This module is maintained using Darcs. You can get the latest version from
+L<http://nothingmuch.woobling.org/Crypt-Util/>, and use C<darcs send> to commit
+changes.
+
+=head1 AUTHORS
+
+Yuval Kogman, E<lt>nothingmuch@woobling.orgE<gt>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 by Yuval Kogman E<lt>nothingmuch@woobling.orgE<gt>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
 =cut
-
-
